665b208f3a66cb5bf3e98b039e5d90bb6aa17344
Sundeep7 <sundeep@wizni.com>
2018-02-27 23:51:32 +0530

    list with filters created...

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  timePeriod: string = "today";
+  itemList: string = "all";
 
   constructor(public navCtrl: NavController) {
 
   }
+
 
 }
